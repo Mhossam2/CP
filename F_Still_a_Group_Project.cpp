4427426d600389d350806780c327ca8f4ef8fe19
Mohanned <mohanned.h2006@gmail.com>
2026-06-25 20:48:25 +0000

    Jun 25, 2026, 11:48 PM

--- F_Still_a_Group_Project.cpp
+++ F_Still_a_Group_Project.cpp
@@ -1,37 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-#define ll long long
-#define fast ios::sync_with_stdio(false); cin.tie(nullptr);
-
+#define HONDA ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+typedef long double ld;
+typedef long long ll;
+#define mpll  map<ll,ll>
+#define vll vector<ll>
+#define all(a) a.begin(), a.end()
+#define fi(ii, n) for (ll i = ii; i < n; i++)
+#define fj(jj, n) for (ll j = jj; j < n; j++)
+#define fit(c) for(auto it = c.begin(); it != c.end(); ++it)
+#define endl "\n"
+int dx[]={1, -1, 0, 0};
+int dy[]={0, 0, 1, -1};
+inline bool in(int i, int j, int rows, int cols){
+    return i>=0 && i<rows && j>=0 && j<cols;
+}
+inline bool in(int i, int l, int h){
+    return i >= l && i <= h;
+}
+using namespace std;
 int main() {
-    fast;
-
-    int t;
+    HONDA
+    int t = 1;
     cin >> t;
-
-    while (t--) {
-        int n, m;
-        cin >> n >> m;
-
-        vector<pair<int, ll>> A, B;
-        A.reserve(n);
-        B.reserve(n);
-
-        vector<ll> a(n), b(n);
-        for (auto &x : a) cin >> x;
-        for (auto &x : b) cin >> x;
-
-        for (int i = 0; i < n; i++) {
-            A.push_back({i % m, a[i]});
-            B.push_back({i % m, b[i]});
+    while(t--){
+        ll n,m;cin>>n>>m;
+        vector<pair<ll,ll>> a,b;
+        fi(0,n){
+            ll num; cin>>num;
+            a.push_back({i%m,num});
         }
-
-        sort(A.begin(), A.end());
-        sort(B.begin(), B.end());
-
-        cout << (A == B ? "YES\n" : "NO\n");
+        fi(0,n){
+            ll num; cin>>num;
+            b.push_back({i%m,num});
+        }
+        sort(all(a));
+        sort(all(b));
+        if(a==b) cout<<"Yes"<<endl;
+        else cout<<"No"<<endl;
     }
-
     return 0;
 }
