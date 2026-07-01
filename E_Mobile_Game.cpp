55da6b0a611f973ee98df4a2503c2d98dfa91771
Mohanned <mohanned.h2006@gmail.com>
2026-07-01 14:35:52 +0000

    Jul 1, 2026, 5:35 PM

---
+++ E_Mobile_Game.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define HONDA                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+typedef long double ld;
+typedef long long ll;
+#define mpll map<ll, ll>
+#define vll vector<ll>
+#define all(a) a.begin(), a.end()
+#define fi(ii, n) for (ll i = ii; i < n; i++)
+#define fj(jj, n) for (ll j = jj; j < n; j++)
+#define fit(c) for (auto it = c.begin(); it != c.end(); ++it)
+#define endl "\n"
+int dx[] = {1, -1, 0, 0};
+int dy[] = {0, 0, 1, -1};
+inline bool in(int i, int j, int rows, int cols)
+{
+    return i >= 0 && i < rows && j >= 0 && j < cols;
+}
+inline bool in(int i, int l, int h)
+{
+    return i >= l && i <= h;
+}
+ll mn;
+bool check(ll mid){
+    return (mid*(mid+1))/2 >= mn; 
+}
+ll bn(){
+    ll lo = 1, hi = 1e18;
+    ll ans = -1;
+    while(lo <= hi){
+        ll mid = lo + (hi - lo) / 2;
+        if(check(mid)){
+            hi = mid - 1;
+            ans = mid;
+        } else {
+            lo = mid + 1;
+        }
+    }
+    return ans;
+}
+using namespace std;
+int main()
+{
+    HONDA
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        ll x,y;cin>>x>>y;
+        mn = min(x,y);
+        cout<<bn()<<endl;
+    }
+    return 0;
+}
