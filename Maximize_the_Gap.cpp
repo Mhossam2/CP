71b3fb6666fce16bd718352b1a2c5eec76733812
Mohanned <mohanned.h2006@gmail.com>
2026-07-01 02:15:05 +0000

    Jul 1, 2026, 5:15 AM

--- Maximize_the_Gap.cpp
+++ Maximize_the_Gap.cpp
@@ -23,6 +23,33 @@ inline bool in(int i, int l, int h)
 {
     return i >= l && i <= h;
 }
+ll k;
+bool check(ll mid, vector<pair<ll,ll>> &a){
+    ll last=-1e9;
+    ll cnt=0;
+    for(auto &p : a){
+        if(p.second - last >= mid){
+            cnt++;
+            last=p.first;
+            if(cnt>=k) return true;
+        }
+    }
+    return cnt >= k;
+}
+ll bn(vector<pair<ll,ll>> &a){
+    ll lo = 1, hi = 1e18;
+    ll ans = -1;
+    while(lo < hi){
+        ll mid = lo + (hi - lo + 1) / 2;
+        if(check(mid,a)){
+            lo = mid;
+            ans = mid;
+        } else {
+            hi = mid - 1;
+        }
+    }
+    return ans;
+}
 using namespace std;
 int main()
 {
@@ -31,13 +58,13 @@ int main()
     //cin >> t;
     while (t--)
     {
-        ll n,k;cin>>n>>k;
+        ll n;cin>>n>>k;
         vector<pair<ll,ll>> a(n);
         fi(0,n){
             cin>>a[i].second>>a[i].first;
         }
         sort(all(a));
-        
+        cout<<bn(a);
     }
     return 0;
 }
