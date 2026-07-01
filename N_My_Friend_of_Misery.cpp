e5f86467c49c8c5bc2471a5e890abcd7c0a13d08
Mohanned <mohanned.h2006@gmail.com>
2026-07-01 16:35:19 +0000

    Jul 1, 2026, 7:35 PM

--- N_My_Friend_of_Misery.cpp
+++ N_My_Friend_of_Misery.cpp
@@ -1,32 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define HONDA                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+typedef long double ld;
+typedef long long ll;
+#define mpll map<ll, ll>
+#define vll vector<ll>
+#define all(a) a.begin(), a.end()
+#define fi(ii, n) for (ll i = ii; i < n; i++)
+#define fj(jj, n) for (ll j = jj; j < n; j++)
+#define fit(c) for (auto it = c.begin(); it != c.end(); ++it)
+#define endl "\n"
+int dx[] = {1, -1, 0, 0};
+int dy[] = {0, 0, 1, -1};
+inline bool in(int i, int j, int rows, int cols)
+{
+    return i >= 0 && i < rows && j >= 0 && j < cols;
+}
+inline bool in(int i, int l, int h)
+{
+    return i >= l && i <= h;
+}
+using namespace std;
+int main()
+{
+    HONDA
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        ll n;cin>>n;
+        ll l=0;
+        ll r=1e18;
+        ll offset = 0;
+        fi(0,n){
+            ll m;char c;cin>>m>>c;
 
-int main(){
-    int T;
-    scanf("%d", &T);
-    while(T--){
-        int N;
-        scanf("%d", &N);
-        long long L = 0;
-        long long U = LLONG_MAX;
-        long long offset = 0;
-        for(int i=0;i<N;i++){
-            long long m;
-            char sign[3];
-            scanf("%lld %s", &m, sign);
-            if(sign[0]=='+'){
-                long long need = m - offset;
-                if(need > L) L = need;
-                offset -= (m + 25);
-            } else {
-                long long lim = m - offset - 1;
-                if(lim < U) U = lim;
-            }
         }
-        if(L < 0) L = 0;
-        long long ans = U - L + 1;
-        if(ans < 0) ans = 0;
-        printf("%lld\n", ans);
     }
     return 0;
 }
