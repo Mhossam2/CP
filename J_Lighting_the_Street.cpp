c922600e149edb36c9101097db80dd6d0339d6b7
Mohanned <mohanned.h2006@gmail.com>
2026-07-01 15:28:51 +0000

    Jul 1, 2026, 6:28 PM

---
+++ J_Lighting_the_Street.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define HONDA                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+typedef long double ld;
+typedef long long ll;
+#define mpll map<ll, ll>
+#define vll vector<ll>
+#define all(a) a.begin(), a.end()
+#define fi(ii, n) for (ll i = ii; i < n; i++)
+#define fj(jj, n) for (ll j = jj; j < n; j++)
+#define fit(c) for (auto it = c.begin(); it != c.end(); ++it)
+#define endl "\n"
+int dx[] = {1, -1, 0, 0};
+int dy[] = {0, 0, 1, -1};
+inline bool in(int i, int j, int rows, int cols)
+{
+    return i >= 0 && i < rows && j >= 0 && j < cols;
+}
+inline bool in(int i, int l, int h)
+{
+    return i >= l && i <= h;
+}
+using namespace std;
+int main()
+{
+    HONDA
+    int t = 1;
+    //cin >> t;
+    while (t--)
+    {
+        ll n,m,l;cin>>n>>m>>l;
+        vll a(n);
+        fi(0,n) cin>>a[i];
+        sort(all(a));
+        ll mx=0;
+        fi(0,n-1){
+            mx=max(abs(a[i]-a[i+1]),mx);
+        }
+        ll mn=1e18;
+        fi(0,m){
+            ll num;cin>>num;
+            if(num>=mx) mn=min(mn,num);
+        }
+        if(mn==1e18) cout<<-1;
+        else cout<<mn;
+    }
+    return 0;
+}
