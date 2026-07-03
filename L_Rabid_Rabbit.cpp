a84cfdcb0cd7696e5ac99ee355fb362071bc0238
Mohanned <mohanned.h2006@gmail.com>
2026-07-03 12:03:59 +0000

    Jul 3, 2026, 3:03 PM

--- L_Rabid_Rabbit.cpp
+++ L_Rabid_Rabbit.cpp
@@ -1,60 +1,70 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int main(){
-    int N,Q;
-    scanf("%d %d",&N,&Q);
-    vector<long long> A(N);
-    long long mx=0;
-    for(int i=0;i<N;i++){ scanf("%lld",&A[i]); mx=max(mx,A[i]); }
-
-    vector<int> P(Q),R(Q);
-    for(int i=0;i<Q;i++) scanf("%d %d",&P[i],&R[i]);
-
-    // value -> sorted indices
-    unordered_map<long long, vector<int>> pos;
-    pos.reserve(N*2);
-    for(int i=0;i<N;i++) pos[A[i]].push_back(i);
-
-    // generate Fibonacci numbers (1,2,3,5,8,...) up to 2*mx
-    vector<long long> fibs;
-    long long a=1,b=2;
-    fibs.push_back(a);
-    fibs.push_back(b);
-    long long limit = 2*mx;
-    while(true){
-        long long c=a+b;
-        if(c>limit) break;
-        fibs.push_back(c);
-        a=b; b=c;
-    }
-
-    vector<int> ans(Q,0);
-    const int INF = INT32_MAX;
-    vector<int> bestJ(N), sufmin(N+1);
-
-    for(long long F: fibs){
-        for(int i=0;i<N;i++){
-            long long target = F - A[i];
-            bestJ[i]=INF;
-            auto it = pos.find(target);
-            if(it!=pos.end()){
-                auto &v = it->second;
-                int lo=0, hi=(int)v.size();
-                while(lo<hi){
-                    int mid=(lo+hi)/2;
-                    if(v[mid]>i) hi=mid; else lo=mid+1;
+#define HONDA                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+typedef long double ld;
+typedef long long ll;
+#define mpll map<ll, ll>
+#define vll vector<ll>
+#define all(a) a.begin(), a.end()
+#define fi(ii, n) for (ll i = ii; i < n; i++)
+#define fj(jj, n) for (ll j = jj; j < n; j++)
+#define fit(c) for (auto it = c.begin(); it != c.end(); ++it)
+#define endl "\n"
+int dx[] = {1, -1, 0, 0};
+int dy[] = {0, 0, 1, -1};
+inline bool in(int i, int j, int rows, int cols)
+{
+    return i >= 0 && i < rows && j >= 0 && j < cols;
+}
+inline bool in(int i, int l, int h)
+{
+    return i >= l && i <= h;
+}
+const ll MOD = 1e9+7;
+using namespace std;
+int main()
+{
+    HONDA
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        ll n,q;cin>>n>>q;
+        vll x(q),l(q),r(q);
+        fi(0,q) cin>>x[i]>>l[i]>>r[i];
+        vector<pair<ll,ll>> c;
+        fi(0,q){
+            fj(i+1,q){
+                if(x[i]==x[j]){
+                    if(l[i]<=r[j] && l[j]<=r[i]){
+                        c.push_back({i,j});
+                    }
                 }
-                if(lo<(int)v.size()) bestJ[i]=v[lo];
+            }
+        }    
+        if(c.size()==0){
+            cout<<"YES"<<endl;
+            continue;
+        }
+        pair<ll,ll> p=c[0];
+        bool f=0;
+        bool f2=0;
+        fi(1,c.size()){
+            if(c[i].first!=p.first && c[i].second != p.first){
+                f=1;
             }
         }
-        sufmin[N]=INF;
-        for(int i=N-1;i>=0;i--) sufmin[i]=min(bestJ[i], sufmin[i+1]);
-
-        for(int q=0;q<Q;q++)
-            if(sufmin[P[q]] <= R[q]) ans[q]++;
+        fi(1,c.size()){
+            if(c[i].first!=p.second && c[i].second != p.second){
+                f2=1;
+            }
+        }
+        if(f&&f2) cout<<"NO"<<endl;
+        else
+        cout<<"YES"<<endl;
     }
-
-    for(int q=0;q<Q;q++) printf("%d\n", ans[q]);
     return 0;
 }
