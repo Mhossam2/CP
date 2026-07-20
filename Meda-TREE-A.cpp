24ab6e05e67741f1e615113cf092b15370f8c8d1
Mohanned Hossam <mohannad.hossam213@gmail.com>
2026-07-20 18:23:58 +0000

    Jul 20, 2026, 9:23 PM

--- Meda-TREE-A.cpp
+++ Meda-TREE-A.cpp
@@ -23,6 +23,30 @@ inline bool in(int i, int l, int h)
 {
     return i >= l && i <= h;
 }
+struct DSU{
+    vector<ll> parent, size;
+    DSU(ll n){
+        parent.resize(n);
+        size.resize(n+1);
+        for(ll i=0; i<n; i++) parent[i]=i;
+    }
+    ll find_set(ll v) {  //find the leader of the set in very small constant of time
+        if (v == parent[v])
+            return v;
+        return parent[v] = find_set(parent[v]);
+    }
+
+    void union_sets(ll a, ll b) { //merge two sets O(1)
+        a = find_set(a);
+        b = find_set(b);
+        if (a != b) {
+            if (size[a] < size[b])
+                swap(a, b);
+            parent[b] = a;
+            size[a] += size[b];
+        }
+    }
+};
 void dfs(ll node, ll parent, vector<vector<ll>> &adj, vector<ll> &color, vector<set<ll>> &ans){
     ans[node].insert(color[node]);
     for(ll nxt: adj[node]){
