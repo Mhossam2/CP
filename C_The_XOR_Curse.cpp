f6b853dabcb302d26f66e7f02ba9c7a65c12daf7
Mohanned Hossam <mohannad.hossam213@gmail.com>
2026-07-25 17:28:05 +0000

    Jul 25, 2026, 8:28 PM

--- C_The_XOR_Curse.cpp
+++ C_The_XOR_Curse.cpp
@@ -33,11 +33,31 @@ int main()
     {
         ll n,k,x;cin>>n>>k>>x;
         vll a(n);
-        vll freq(1024,0);
-        fi(0,n) cin>>a[i], freq[a[i]]++;
+        ll mx=0;
+        ll mn = 1e5;
+        vector<ll> freq(1024,0);
+        fi(0,n) cin>>a[i], freq[a[i]]++, mx=max(mx,a[i]), mn=min(mn,a[i]);
+        ll ind=0;
+        vector<ll> newfreq(1024,0);
         fi(0,k){
-            vll newfreq(1024,0);
+            ind=0;
+            fj(0,1024) newfreq[j]=0;
+            for(ll j = 0;j<1024;j++){
+                ll newnum = j ^ x;
+                if(ind%2 == 0){
+                    newfreq[newnum] += freq[j]/2;
+                    newfreq[j] += freq[j]/2 + 1;
+                }
+                else{
+                    newfreq[newnum] += freq[j] / 2 + 1;
+                    newfreq[j] += freq[j] / 2;
+                }
+                if(newfreq[newnum]) mx=max(mx,newnum), mn=min(mn,newnum);
+                ind += freq[j];
+            }
+            freq=newfreq;
         }
+        cout<<mx<<" "<<mn;
         
     }
     return 0;
