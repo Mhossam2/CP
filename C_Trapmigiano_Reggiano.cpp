fa21ef1609735bc73970f6e2f1c5ede31cfdad81
Mohanned Hossam <mohannad.hossam213@gmail.com>
2026-07-28 11:19:25 +0000

    Jul 28, 2026, 2:19 PM

--- C_Trapmigiano_Reggiano.cpp
+++ C_Trapmigiano_Reggiano.cpp
@@ -27,56 +27,10 @@ int main()
 {
     HONDA
     int t = 1;
-    //cin >> t;
+    cin >> t;
     while (t--)
     {
-        ll n;cin>>n;
-        vll color(n+1);
-        fi(1,n+1){
-            cin>>color[i];
-        }  
-        vector<vector<ll>> adj(n+1);
-        fi(0,n-1){
-            ll x,y;cin>>x>>y;
-            adj[x].push_back(y);
-            adj[y].push_back(x);
-        }
-        vector<ll> p(n+1,0);
-        vector<ll> path;
-        vector<bool> vis(n+1,false);
-        queue<ll> q;
-        q.push(1);
-        vis[1]=1;
-        while(!q.empty()){
-            ll node = q.front(); q.pop();
-            path.push_back(node);
-            for(ll nxt : adj[node]){
-                if(vis[nxt]) continue;
-                vis[nxt] = 1;
-                p[nxt] = node;
-                q.push(nxt);
-            }
-        }
-        vector<vector<ll>> c(n+1);
-        for(ll u: path){
-            if(u!=1) c[p[u]].push_back(u);
-        }
-        vector<set<ll>> s(n+1);
-        vector<ll> ans(n+1);
-        for(ll i = path.size()-1; i>=0; i--){
-            ll u = path[i];
-            s[u].insert(color[u]);
-            for(ll x: c[u]){
-                if(s[x].size() > s[u].size()){
-                    s[u].swap(s[x]);
-                }
-                for(ll v:s[x]) s[u].insert(v);
-            }
-            ans[u]=s[u].size();
-        }
-        fi(1,n+1){
-            cout<<ans[i]<<" ";
-        }
+        
     }
     return 0;
 }
