4d34387898a9467710490fa585c708b968eb2c5d
Mohanned Hossam <mohannad.hossam213@gmail.com>
2026-07-30 17:20:47 +0000

    Jul 30, 2026, 8:20 PM

---
+++ F_Mostafa_Edress_Loves_Strings.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define HONDA                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+typedef long double ld;
+typedef long long ll;
+#define mpll map<ll, ll>
+#define vll vector<ll>
+#define all(a) a.begin(), a.end()
+#define fi(ii, n) for (ll i = ii; i < n; i++)
+#define fj(jj, n) for (ll j = jj; j < n; j++)
+#define fit(c) for (auto it = c.begin(); it != c.end(); ++it)
+#define endl "\n"
+int dx[] = {1, -1, 0, 0};
+int dy[] = {0, 0, 1, -1};
+inline bool in(int i, int j, int rows, int cols)
+{
+    return i >= 0 && i < rows && j >= 0 && j < cols;
+}
+inline bool in(int i, int l, int h)
+{
+    return i >= l && i <= h;
+}
+int main()
+{
+    HONDA
+    int t = 1;
+    //cin >> t;
+    while (t--)
+    {
+        ll n,k;cin>>n>>k;
+        if(k>n || (k==1 && n>1)){
+            cout<<-1<<endl;
+            continue;
+        }
+        string s="";
+        while(s.size()<n){
+            fi(0,k){
+                s += ('a'+i);
+                if(s.size()==n) break;
+            }
+        }
+        cout<<s;
+    }
+    return 0;
+}
