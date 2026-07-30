8d7de6c8ef59c4343e6586d5fe2908ec7b083c40
Mohanned Hossam <mohannad.hossam213@gmail.com>
2026-07-30 17:32:29 +0000

    Jul 30, 2026, 8:32 PM

---
+++ A_Signal_product.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define HONDA                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+typedef long double ld;
+typedef long long ll;
+#define mpll map<ll, ll>
+#define vll vector<ll>
+#define all(a) a.begin(), a.end()
+#define fi(ii, n) for (ll i = ii; i < n; i++)
+#define fj(jj, n) for (ll j = jj; j < n; j++)
+#define fit(c) for (auto it = c.begin(); it != c.end(); ++it)
+#define endl "\n"
+int dx[] = {1, -1, 0, 0};
+int dy[] = {0, 0, 1, -1};
+inline bool in(int i, int j, int rows, int cols)
+{
+    return i >= 0 && i < rows && j >= 0 && j < cols;
+}
+inline bool in(int i, int l, int h)
+{
+    return i >= l && i <= h;
+}
+int main()
+{
+    HONDA
+    int t = 1;
+    //cin >> t;
+    while (t--)
+    {
+        ll n;
+        vll a(n);
+        fi(0,n) cin>>a[i];
+        ll ans=-1e18;
+        for(ll i=1;i<11 && i<n;i++){
+            ll num = 1;
+            ll l=0;
+            ll r=0;
+            while(r<n && r<=i){
+                num*=a[r];
+            }
+            ans=max(num,ans);
+            while(r<n){
+                num*=a[r];
+                num/a[l];
+                ans=max(num,ans);
+            }
+        }
+        cout<<ans;
+    }
+    return 0;
+}
