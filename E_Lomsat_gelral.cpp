5c672b6d2423fb47c9af72512ab496e4857f5ffc
Mohanned Hossam <mohannad.hossam213@gmail.com>
2026-08-10 17:58:57 +0000

    Aug 10, 2026, 8:58 PM

--- E_Lomsat_gelral.cpp
+++ E_Lomsat_gelral.cpp
@@ -23,59 +23,95 @@ inline bool in(int i, int l, int h)
 {
     return i >= l && i <= h;
 }
-vector<ll> a;
-vector<vector<ll>> adj;
-ll ans;
-vector<map<ll, ll>> cnt;  //cnt[v][c] = number of top level vertices that have color c on v's subtree
-// cnt[v][c] = sum of cnt[u][c] for all childs
-void dfs(ll v, ll p)
+const int OO = 1e9 + 5;
+const int N = 2e5 + 5;
+
+vll adj[N];
+ll c[N], sz[N], big[N];
+ll freq[N], mx;
+ll sum[N], ans[N];
+
+void pre(ll v, ll p) //to calculate the subtree size and the heavy child
 {
+    sz[v] = 1;
     for (ll u : adj[v])
         if (u != p)
         {
-            dfs(u, v);
-            if (cnt[v].size() < cnt[u].size())
-            {
-                swap(cnt[v], cnt[u]);
-            }
-            for (auto [x, y] : cnt[u])
-            {
-                if (x != a[v])
-                {
-                    ans += cnt[v][x] * y;
-                }
-                cnt[v][x] += y;
-            }
+            pre(u, v);
+            sz[v] += sz[u];
+            if (big[v] == 0 || sz[u] > sz[big[v]])
+                big[v] = u;
         }
-    ans += cnt[v][a[v]];
-    cnt[v][a[v]] = 1;
+}
+
+void upd(ll col, ll d) // the function you update the most depending on the question
+{
+    if (freq[col] + d > mx)
+        mx++;
+    else if (freq[col] == mx && sum[freq[col]] == col)
+        mx--;
+
+    sum[freq[col]] -= col;
+    freq[col] += d;
+    sum[freq[col]] += col;
+}
+
+void collect(ll v, ll p, ll d) //merging all light childs
+{
+    upd(c[v], d);
+    for (ll u : adj[v])
+        if (u != p)
+        {
+            collect(u, v, d);
+        }
+}
+
+void dfs(ll v, ll p, bool keep)
+{
+    for (ll u : adj[v])
+        if (u != p && u != big[v])
+        {
+            dfs(u, v, false);
+        }
+    // add to DS
+    if (big[v] != 0)
+        dfs(big[v], v, true);
+    upd(c[v], 1);
+    for (int u : adj[v])
+        if (u != p && u != big[v])
+            collect(u, v, +1); // light/small subtrees
+
+    // answer queries
+    ans[v] = sum[mx];
+
+    // remove from DS
+    if (!keep)
+        collect(v, p, -1);
 }
 int main()
 {
     HONDA
     int t = 1;
-    cin >> t;
+    // cin >> t;
     while (t--)
     {
         ll n;
         cin >> n;
+        for (ll i = 1; i <= n; ++i)
+            cin >> c[i];
 
-        a.resize(n);
-        fi(0, n) cin >> a[i];
-        adj.assign(n, {});
-        fi(0, n - 1)
+        ll a, b;
+        for (int i = 0; i < n - 1; ++i)
         {
-            ll u, v;
-            cin >> u >> v;
-            u--;
-            v--;
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+            cin >> a >> b;
+            adj[a].push_back(b);
+            adj[b].push_back(a);
         }
-        ans = 0;
-        cnt.assign(n, {});
-        dfs(0, -1);
-        cout << ans << endl;
+
+        pre(1, 1);
+        dfs(1, 1, true);
+        for (ll i = 1; i <= n; ++i)
+            cout << ans[i] << ' ';
     }
     return 0;
 }
