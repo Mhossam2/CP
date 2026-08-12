d17aae843c26e2729547ae66f6ada4fae02b9d5a
Mohanned Hossam <mohannad.hossam213@gmail.com>
2026-08-12 19:16:12 +0000

    Aug 12, 2026, 10:16 PM

--- M_Meridian_Colony.cpp
+++ M_Meridian_Colony.cpp
@@ -34,41 +34,53 @@ int main()
     //cin >> t;
     while (t--)
     {
-        ll n,q;cin>>n>>q;
-        vll w(n+1);
-        vll a(n+1);
-        for(ll i=1;i<n+1;i++){
-            cin>>a[i];
-        } 
-        for(ll i=1;i<=n;i++){
-            cin>>w[i];
+    ll n; cin >> n;
+    vector<vector<ll>> graph(n + 1);
+    for(ll i = 0, u, v; i < n - 1; i++){
+        cin >> u >> v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+    vector<ll> tin(n + 1), tout(n + 1), vertex(n + 1), sz(n + 1, 1), d(n + 1, 1);
+    ll timer = 0;
+    function<ll(ll, ll)> euler =[&](ll u, ll p){
+        tin[u] = ++timer;
+        vertex[tin[u]] = u;
+        for(auto v : graph[u]) if(v != p){
+            d[v] = d[u] + 1;
+            sz[u] += euler(v, u);
         }
-        vector<vector<ll>> adj(n+1);
-        for(ll i=0;i<n-1;i++){
-            ll x,y;cin>>x>>y;
-            adj[y].push_back(x);
-            adj[x].push_back(y);
+        tout[u] = timer;
+        return sz[u];
+    };
+    euler(1, -1);
+
+    auto add =[&](ll u){};
+    auto remove =[&](ll u){};
+
+    function<void(ll, ll, bool)> dfs =[&](ll u, ll p, bool keep){
+        ll big = -1;
+        for(auto v : graph[u]){
+            if(v == p) continue;
+            if(big == -1 || sz[v] > sz[big]) big = v;
         }
-        vector<ll> dis,In(n+1),Out(n+1);
-        auto dfs = [&](auto &&self,ll node, ll parent) -> void
-        {
-            dis.push_back(node);
-            In[node]=dis.size();
-            for(ll ch:adj[node]){
-                if(ch==parent) continue;
-                self(self,ch,node);
+        for(auto v : graph[u]) 
+            if(v != p && v != big) dfs(v, u, 0);
+        if(big != -1) dfs(big, u, 1);
+        for(auto v : graph[u]){
+            if(v == p || v == big) continue;
+            for(ll i = tin[v]; i <= tout[v]; i++){
+                // here
+                add(vertex[i]);
             }
-            Out[node]=dis.size();
-        };
-        dfs(dfs,1,0);
-        vector<Query> queries(q);
-        for(ll i =0;i<q;i++){
-            ll u;cin>>u;
-            queries[i].l=In[u];
-            queries[i].r=Out[u];
-            cin>>queries[i].x>>queries[i].y;
         }
-        
+        add(u);
+        if(!keep){
+            for(ll i = tin[u]; i <= tout[u]; i++) remove(vertex[i]);
+        }
+    };
+    dfs(1, -1, 1);
+
     }
     return 0;
 }
