a62e673703c863f1085fb47dbff7f50b1384af57
Mohanned Hossam <mohannad.hossam213@gmail.com>
2026-08-15 20:09:01 +0000

    Aug 15, 2026, 11:09 PM

--- G_Cursed_Library.cpp
+++ G_Cursed_Library.cpp
@@ -62,3 +62,61 @@ struct segtree{
         return merge(ln, rn);
     }
 };
+int main(){
+    ll n,q; cin >> n >> q;
+    vector<ll> a(n+1,0);
+    for(ll i=1; i<=n; i++) cin>>a[i];
+    vector<vector<ll>> graph(n + 1);
+    for(ll i = 0, u, v; i < n - 1; i++){
+        cin >> u >> v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+    vector<ll> tin(n + 1), tout(n + 1), vertex(n + 1), sz(n + 1, 1), d(n + 1, 1);
+    ll timer = 0;
+    function<ll(ll, ll)> euler =[&](ll u, ll p){
+        tin[u] = ++timer;
+        vertex[tin[u]] = u;
+        for(auto v : graph[u]) if(v != p){
+            d[v] = d[u] + 1;
+            sz[u] += euler(v, u);
+        }
+        tout[u] = timer;
+        return sz[u];
+    };
+    euler(1, -1);
+    vector<ll> freq(n+1, 0);
+    segtree seg = segtree(freq);
+
+    auto add =[&](ll u){
+        freq[a[u]]++;
+        if(freq[a[u]]==1) seg.update(a[u],1,0,0,seg.treesize);
+    };
+    auto remove =[&](ll u){
+        freq[a[u]]--;
+        if(freq[a[u]]==0) seg.update(a[u],0,0,0,seg.treesize);
+    };
+
+    function<void(ll, ll, bool)> dfs =[&](ll u, ll p, bool keep){
+        ll big = -1;
+        for(auto v : graph[u]){
+            if(v == p) continue;
+            if(big == -1 || sz[v] > sz[big]) big = v;
+        }
+        for(auto v : graph[u]) 
+            if(v != p && v != big) dfs(v, u, 0);
+        if(big != -1) dfs(big, u, 1);
+        for(auto v : graph[u]){
+            if(v == p || v == big) continue;
+            for(ll i = tin[v]; i <= tout[v]; i++){
+                // here
+                add(vertex[i]);
+            }
+        }
+        add(u);
+        if(!keep){
+            for(ll i = tin[u]; i <= tout[u]; i++) remove(vertex[i]);
+        }
+    };
+    dfs(1, -1, 1);
+}
