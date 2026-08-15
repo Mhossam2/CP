f09df1ca4fdfb7d1161392546ff4db26fe7f6cae
Mohanned Hossam <mohannad.hossam213@gmail.com>
2026-08-15 20:47:43 +0000

    Aug 15, 2026, 11:47 PM

--- G_Cursed_Library.cpp
+++ G_Cursed_Library.cpp
@@ -25,6 +25,13 @@ struct Node
         best = pre = suf = max(0ll, val);
     }
 };
+
+// 2 3 1 5 4
+// - - - - -
+// - - 1 - -
+// 2 - 1 - -
+// 2 3 1 - -
+// - - 1 - -
 struct segtree
 {
     ll treesize;
